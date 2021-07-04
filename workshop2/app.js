39d459275eae40c665c6e9722a7a95ffb188da02
Asgedom <63181283+Asget-dev@users.noreply.github.com>
2021-07-04 07:02:42 -0700

    updated post method

--- workshop2/app.js
+++ workshop2/app.js
@@ -2,16 +2,37 @@ fetch("http://localhost:3000/api/v1/books")
   .then((res) => res.json())
   .then((data) => {
     let x = data.result;
-    // console.log(x[0].id)
+    // console.log(x[1].id)
     if (data.status === "success") {
       for (let i = 0; i < x.length; i++) {
-        document.querySelectorAll(".allBooks1").innerHTML = x[i].id;
-        document.querySelectorAll(".allBooks2").innerHTML = x[i].title;
-        document.querySelectorAll(".allBooks3").innerHTML = x[i].isbn;
-        document.querySelectorAll(".allBooks4").innerHTML = x[i].publishDate;
-        document.querySelectorAll(".allBooks5").innerHTML = x[i].author;
+        document.querySelector(".allBooks1").innerHTML=x[i].id;
+        document.querySelector(".allBooks2").innerHTML = x[i].title;
+        document.querySelector(".allBooks3").innerHTML = x[i].isbn;
+        document.querySelector(".allBooks4").innerHTML = x[i].publishDate;
+        document.querySelector(".allBooks5").innerHTML = x[i].author;
       }
     } else {
       document.querySelector(".allBooks").innerHTML = "null";
     }
   });
+
+  document.querySelector("#button").addEventListener('click',()=>{
+      const book = {};
+      book.id = document.querySelector("#bookid").value
+      book.title = document.querySelector("#title").value
+      book.isbn = document.querySelector("#isbn").value
+      book.publishDate = document.querySelector("#publishDate").value
+      book.author = document.querySelector("#author").value
+console.log(book)
+      fetch('http://localhost:3000/api/v1/books',{
+          headers:{
+              'Content-Type' : 'Application/json'
+          },
+          method: 'POST',
+          body:JSON.stringify(book)
+      })
+      .then(res=>res.json())
+      .then(data=>{
+          console.log(data)
+      })
+  })
